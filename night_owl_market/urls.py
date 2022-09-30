fbf490577b3d1060124128ffd96fd46567eb1a7a
DinhHuy <huyn27316@gmail.com>
2022-09-30 09:57:58 +0700

    add blacklist endpoint

--- night_owl_market/urls.py
+++ night_owl_market/urls.py
@@ -21,6 +21,7 @@ from drf_yasg import openapi
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView,
 )
 
 schema_view = get_schema_view(
@@ -44,4 +45,5 @@ urlpatterns = [
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
